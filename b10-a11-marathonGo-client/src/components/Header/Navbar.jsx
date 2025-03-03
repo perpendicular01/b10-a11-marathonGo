c9257818dbbe1c6e7ba9b8e636277419f8b7698e
saif <kh.s.k.rohan@gmail.com>
2025-03-03 11:01:12 +0600

    update navbar

--- b10-a11-marathonGo-client/src/components/Header/Navbar.jsx
+++ b10-a11-marathonGo-client/src/components/Header/Navbar.jsx
@@ -26,8 +26,29 @@ const Navbar = () => {
         catch { /* empty */ }
     }
 
-    // links
-    const links = <>
+    // for small and middle  BEFORE LOGIN
+    const links1 = <>
+        <li>
+            <NavLink
+                to="/"
+                className={({ isActive }) => isActive ? "font-bold" : "hover:font-semibold"}
+            >
+                Home
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to="/allMarathons"
+                className={({ isActive }) => isActive ? "font-bold" : "hover:font-semibold"}
+            >
+                All Marathons
+            </NavLink>
+        </li>
+
+    </>
+
+    // AFTER LOGIN SM AND MD
+    const links2 = <>
         <li>
             <NavLink
                 to="/"
@@ -71,29 +92,130 @@ const Navbar = () => {
 
     </>
 
+    // before login 
+    const links3 = <>
+        <li>
+            <NavLink
+                to="/"
+                className={({ isActive }) => isActive ? "font-bold" : "hover:font-semibold"}
+            >
+                Home
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to="/allMarathons"
+                className={({ isActive }) => isActive ? "font-bold" : "hover:font-semibold"}
+            >
+                All Marathons
+            </NavLink>
+        </li>
+
+
+    </>
+
+    // AFTER LOGIN IN 
+    const links4 = (
+        <>
+          <li>
+            <NavLink to="/" className={({ isActive }) => (isActive ? "font-bold" : "hover:font-semibold")}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/allMarathons" className={({ isActive }) => (isActive ? "font-bold" : "hover:font-semibold")}>
+              All Marathons
+            </NavLink>
+          </li>
+          {/* Dashboard Dropdown */}
+          <li>
+            <div className="dropdown">
+              <div tabIndex={0} role="button" className="">
+                <h2>Dashboard</h2>
+              </div>
+              <ul tabIndex={0} className="menu menu-base dropdown-content bg-base-100 rounded-box z-10 mt-3 w-64 p-2 shadow">
+                <li>
+                  <NavLink to="/addMarathon" className={({ isActive }) => (isActive ? "font-bold" : "hover:font-semibold")}>
+                    Add Marathon
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/myMarathonList"
+                    className={({ isActive }) => (isActive ? "font-bold" : "hover:font-semibold")}
+                  >
+                    My Marathon List
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/myApplyList" className={({ isActive }) => (isActive ? "font-bold" : "hover:font-semibold")}>
+                    My Apply List
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </>
+      );
+      
+
     return (
         <div className="bg-[#E28377] dark:bg-gray-900 ">
             <div className="w-[95%] lg:w-[90%] mx-auto flex justify-between items-center py-2 md:py-2 lg:py-3   text-black">
                 {/* for small device only */}
-                <div className="lg:hidden">
+
+                {
+                    user ?
+                        <div className="lg:hidden">
+                            <div className="dropdown ">
+                                <div tabIndex="0" role="button" className="text-3xl m-1"><IoIosMenu></IoIosMenu></div>
+                                <ul tabIndex="0" className="dropdown-content menu bg-base-100 rounded-box z-[1] w-40 p-2 shadow">
+                                    {links2}
+                                </ul>
+                            </div>
+                        </div>
+
+                        :
+
+                        <div className="lg:hidden">
+                            <div className="dropdown ">
+                                <div tabIndex="0" role="button" className="text-3xl m-1"><IoIosMenu></IoIosMenu></div>
+                                <ul tabIndex="0" className="dropdown-content menu bg-base-100 rounded-box z-[1] w-40 p-2 shadow">
+                                    {links1}
+                                </ul>
+                            </div>
+                        </div>
+                }
+
+                {/* <div className="lg:hidden">
                     <div className="dropdown ">
                         <div tabIndex="0" role="button" className="text-3xl m-1"><IoIosMenu></IoIosMenu></div>
                         <ul tabIndex="0" className="dropdown-content menu bg-base-100 rounded-box z-[1] w-40 p-2 shadow">
                             {links}
                         </ul>
                     </div>
-                </div>
+                </div> */}
 
                 {/* left */}
                 <div>
                     <Link to={'/'}>
-                `       <h2 className="-mt-7 text-lg lg:text-2xl dark:text-white font-bold ">MarathonGoo</h2>
+                        `       <h2 className="-mt-7 text-lg lg:text-2xl dark:text-white font-bold ">MarathonGoo</h2>
                     </Link>
                 </div>
                 {/* for md middle */}
-                <div className="hidden list-none lg:flex items-center gap-5 text-sm lg:text-base dark:text-gray-300 ">
-                    {links}
-                </div>
+                {
+                    user ?
+                        <div className="hidden list-none lg:flex items-center gap-5 text-sm lg:text-base dark:text-gray-300 ">
+                            {links4}
+                        </div>
+
+                        :
+
+                        <div className="hidden list-none lg:flex items-center gap-5 text-sm lg:text-base dark:text-gray-300 ">
+                            {links3}
+                        </div>
+                }
+
                 {/* right side */}
                 {/* <div className="flex items-center gap-2 md:gap-5 ">
                     <Link to="/auth/login"><button className={`px-2 md:px-4 py-2 rounded-lg text-sm md:text-base  font-medium ${path === '/auth/login' ? "text-white bg-[#6C5B1D]" : "bg-[#FFF2C2] text-base"}`}>  Login </button></Link>
@@ -113,8 +235,8 @@ const Navbar = () => {
                                         <button
                                             onClick={handleSignOut}
                                             className={`px-2 md:px-4 py-2 rounded-lg text-sm md:text-base font-medium ${path === '/auth/login'
-                                                    ? 'text-black bg-[#A8A8A8] '
-                                                    : 'bg-[#183155] text-white hover:bg-[#455367]'
+                                                ? 'text-black bg-[#A8A8A8] '
+                                                : 'bg-[#183155] text-white hover:bg-[#455367]'
                                                 }`}
                                         >
                                             Logout
